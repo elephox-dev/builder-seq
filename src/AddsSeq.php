ebb46e1d42d1a360577710ec49def94706214499
Ricardo Boss <contact@ricardoboss.de>
2023-06-18 12:42:49 +0200

    Implement AddsSeq trait with ricardoboss/php-seq

--- src/AddsSeq.php
+++ src/AddsSeq.php
@@ -3,11 +3,89 @@ declare(strict_types=1);
 
 namespace Elephox\BuilderSeq;
 
+use Elephox\Configuration\Contract\Configuration;
+use Elephox\DI\Contract\ServiceCollection;
+use Elephox\DI\ServiceNotFoundException;
+use Elephox\Logging\MultiSinkLogger;
+use Elephox\Web\ConfigurationException;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Log\LoggerInterface;
+use RicardoBoss\PhpSeq\Contract\SeqClient;
+use RicardoBoss\PhpSeq\Contract\SeqLogger as SeqLoggerInterface;
+use RicardoBoss\PhpSeq\SeqHttpClient;
+use RicardoBoss\PhpSeq\SeqHttpClientConfiguration;
+use RicardoBoss\PhpSeq\SeqLogger;
+use RicardoBoss\PhpSeq\SeqLoggerConfiguration;
+
 trait AddsSeq {
-	public function addSeq(): self
+	abstract protected function getServices(): ServiceCollection;
+
+	public function addHttpSeqClient(): void
 	{
+		foreach ([ClientInterface::class, RequestFactoryInterface::class, StreamFactoryInterface::class] as $service) {
+			if (!$this->getServices()->has($service)) {
+				throw $this->missingService($service, "This is required for HTTP ingestion.");
+			}
+		}
 
+		$this->getServices()->tryAddSingleton(SeqHttpClientConfiguration::class, factory: function (Configuration $config) {
+			return new SeqHttpClientConfiguration(
+				$config["seq:http:endpoint"] ?? throw new ConfigurationException("Missing required configuration key: seq:http:endpoint"),
+				$config["seq:http:api-key"] ?? null,
+				$config["seq:http:max-retries"] ?? 3,
+			);
+		});
 
-		return $this;
+		$this->getServices()->tryAddSingleton(SeqClient::class, SeqHttpClient::class);
+	}
+
+	public function addSeqLogger(bool $replaceExisting = false): void
+	{
+		if (!$this->getServices()->has(SeqClient::class)) {
+			throw $this->missingService(SeqClient::class, "Please add a client before adding the logger.");
+		}
+
+		$this->getServices()->tryAddSingleton(SeqLoggerConfiguration::class, factory: function (Configuration $config) {
+			return new SeqLoggerConfiguration(
+				$config["seq:logger:backlog-limit"] ?? 50,
+				$config["seq:logger:context"] ?? null,
+				$config["seq:logger:minimum-log-level"] ?? null,
+			);
+		});
+
+		$this->getServices()->tryAddSingleton(SeqLoggerInterface::class, SeqLogger::class);
+		$this->getServices()->addSingleton(SeqSink::class, SeqSink::class);
+
+		if ($this->getServices()->has(LoggerInterface::class)) {
+			$logger = $this->getServices()->get(LoggerInterface::class);
+			if ($logger instanceof MultiSinkLogger) {
+				$seqSink = $this->getServices()->get(SeqSink::class);
+				$logger->addSink($seqSink);
+
+				return;
+			}
+
+			if (!$replaceExisting) {
+				$logger->error("Unable to add Seq logger to a non-MultiSink logger without replacing it.");
+
+				return;
+			}
+		}
+
+		// replaces any existing logger
+		$this->getServices()->addSingleton(LoggerInterface::class, factory: function (SeqSink $sink) {
+			$logger = new MultiSinkLogger();
+
+			$logger->addSink($sink);
+
+			return $logger;
+		});
+	}
+
+	private function missingService(string $service, string $message): ServiceNotFoundException
+	{
+		return new ServiceNotFoundException("No '$service' service found. $message");
 	}
 }
--- src/SeqSink.php
+++ src/SeqSink.php
@@ -6,14 +6,19 @@ namespace Elephox\BuilderSeq;
 use Elephox\Logging\Contract\LogLevel;
 use Elephox\Logging\Contract\Sink;
 use Elephox\Logging\SinkCapability;
+use RicardoBoss\PhpSeq\Contract\SeqLogger;
 
-class SeqSink implements Sink
+readonly class SeqSink implements Sink
 {
+	public function __construct(
+		private SeqLogger $logger,
+	) {}
+
 	public function hasCapability(SinkCapability $capability): bool {
-		// TODO: Implement hasCapability() method.
+		return $capability === SinkCapability::MessageTemplates;
 	}
 
-	public function write(LogLevel $level, string $message, array $context): void {
-		// TODO: Implement write() method.
+	public function write(LogLevel $level, string $message, ?array $context): void {
+		$this->logger->log($level, $message, $context);
 	}
 }
